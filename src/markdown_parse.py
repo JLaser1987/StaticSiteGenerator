eed5c5e2669093d184cd507b7918657ded83bb07
Lasolimu <Lasolimu@gmail.com>
2026-04-02 14:46:51 -0600

    Start parsing nested segments of markdown

--- src/markdown_parse.py
+++ src/markdown_parse.py
@@ -1,4 +1,14 @@
+import re
+from enum import Enum
 from textnode import TextNode, TextType
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     if delimiter == "" or delimiter is None:
@@ -19,3 +29,94 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
         else:
             newNodes.append(node)
     return newNodes
+
+def extract_markdown_images(text):
+    return re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+
+def extract_markdown_links(text):
+    return re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+
+def split_nodes_image(old_nodes):
+    newNodes = []
+    for node in old_nodes:
+        if isinstance(node, TextNode):
+            images = extract_markdown_images(node.text)
+            if len(images) == 0:
+                newNodes.append(node)
+                continue
+            textFragments = re.split(r"!\[(?:.*?)\]\((.*?)\)", node.text)
+            currentImage = 0
+            flagged = False
+            for fragment in textFragments:
+                if fragment == "":
+                    continue
+                if not flagged or currentImage >= len(images):
+                    newNodes.append(TextNode(fragment, node.textType, node.url))
+                else:
+                    newNodes.append(TextNode(images[currentImage][0], TextType.IMAGE, images[currentImage][1]))
+                    currentImage += 1
+                flagged = not flagged
+        else:
+            newNodes.append(node)
+    return newNodes
+
+def split_nodes_link(old_nodes):
+    newNodes = []
+    for node in old_nodes:
+        if isinstance(node, TextNode):
+            links = extract_markdown_links(node.text)
+            if len(links) == 0:
+                newNodes.append(node)
+                continue
+            textFragments = re.split(r"\[(?:.*?)\]\((.*?)\)", node.text)
+            currentLink = 0
+            flagged = False
+            for fragment in textFragments:
+                if fragment == "":
+                    continue
+                if not flagged or currentLink >= len(links):
+                    newNodes.append(TextNode(fragment, node.textType, node.url))
+                else:
+                    newNodes.append(TextNode(links[currentLink][0], TextType.LINK, links[currentLink][1]))
+                    currentLink += 1
+                flagged = not flagged
+        else:
+            newNodes.append(node)
+    return newNodes
+
+def splitText(text):
+    nodes = [TextNode(text, TextType.TEXT)]
+    nodes = split_nodes_delimiter(nodes, "**", TextType.BOLD)
+    nodes = split_nodes_delimiter(nodes, "_", TextType.ITALIC)
+    nodes = split_nodes_delimiter(nodes, "`", TextType.CODE)
+    nodes = split_nodes_image(nodes)
+    nodes = split_nodes_link(nodes)
+    return nodes
+
+def markdownToBlocks(markdown):
+    blocks = markdown.split("\n\n")
+    finalBlocks = []
+    for block in blocks:
+        stripped= block.strip()
+        if stripped != "":
+            finalBlocks.append(stripped)
+    return finalBlocks
+
+def blockToBlockType(block):
+    if block.startswith('#'):
+         return BlockType.HEADING
+    elif block.startswith("```\n") and block.endswith("```"):
+        return BlockType.CODE
+
+    lines = block.split("\n")
+    type = BlockType.PARAGRAPH
+    for i in range(len(lines)):
+        if lines[i].startswith('>') and (type == BlockType.PARAGRAPH or type == BlockType.QUOTE):
+            type = BlockType.QUOTE
+        elif lines[i].startswith("- ") and (type == BlockType.PARAGRAPH or type == BlockType.UNORDERED_LIST):
+            type = BlockType.UNORDERED_LIST
+        elif lines[i].startswith(f"{i+1}. ") and (type == BlockType.PARAGRAPH or type == BlockType.ORDERED_LIST):
+            type = BlockType.ORDERED_LIST
+        else:
+            return BlockType.PARAGRAPH
+    return type
--- src/test_markdownparse.py
+++ src/test_markdownparse.py
@@ -1,7 +1,7 @@
 import unittest
 
 from textnode import TextNode, TextType
-from markdown_parse import split_nodes_delimiter
+from markdown_parse import split_nodes_delimiter, extract_markdown_images, extract_markdown_links, split_nodes_image, split_nodes_link, splitText, markdownToBlocks, blockToBlockType, BlockType
 
 
 class TestMarkdownParse(unittest.TestCase):
@@ -42,6 +42,156 @@ class TestMarkdownParse(unittest.TestCase):
                         TextNode(" word", TextType.TEXT),
                     ]
         self.assertEqual(new_nodes, expectedNew)
+
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+    def test_extract_markdown_links(self):
+        linkURL = "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"
+        matches = extract_markdown_links(
+            f"This is text with a [link for]({linkURL})"
+        )
+        self.assertListEqual([("link for", linkURL)], matches)
+
+    def test_split_images(self):
+        node = TextNode(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_image([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with an ", TextType.TEXT),
+                TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+                TextNode(" and another ", TextType.TEXT),
+                TextNode(
+                    "second image", TextType.IMAGE, "https://i.imgur.com/3elNhQu.png"
+                ),
+            ],
+            new_nodes,
+        )
+
+    def test_split_links(self):
+        node = TextNode(
+            "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_link([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with a link ", TextType.TEXT),
+                TextNode("to boot dev", TextType.LINK, "https://www.boot.dev"),
+                TextNode(" and ", TextType.TEXT),
+                TextNode(
+                    "to youtube", TextType.LINK, "https://www.youtube.com/@bootdotdev"
+                ),
+            ],
+            new_nodes,
+        )
+
+    def test_full_split(self):
+        text = 'This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)'
+        newNodes = splitText(text)
+        expected = [
+                        TextNode("This is ", TextType.TEXT),
+                        TextNode("text", TextType.BOLD),
+                        TextNode(" with an ", TextType.TEXT),
+                        TextNode("italic", TextType.ITALIC),
+                        TextNode(" word and a ", TextType.TEXT),
+                        TextNode("code block", TextType.CODE),
+                        TextNode(" and an ", TextType.TEXT),
+                        TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+                        TextNode(" and a ", TextType.TEXT),
+                        TextNode("link", TextType.LINK, "https://boot.dev"),
+                    ]
+        self.assertListEqual(expected, newNodes)
+
+    def test_full_split_nested(self):
+        text = 'This is **text with an _italic_ word** and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)'
+        newNodes = splitText(text)
+        expected = [
+                        TextNode("This is ", TextType.TEXT),
+                        TextNode("text with an ", TextType.BOLD),
+                        TextNode("italic", TextType.ITALIC),
+                        TextNode(" word", TextType.BOLD),
+                        TextNode(" and a ", TextType.TEXT),
+                        TextNode("code block", TextType.CODE),
+                        TextNode(" and an ", TextType.TEXT),
+                        TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+                        TextNode(" and a ", TextType.TEXT),
+                        TextNode("link", TextType.LINK, "https://boot.dev"),
+                    ]
+        self.assertListEqual(expected, newNodes)
+
+    def test_markdown_to_blocks(self):
+        md = """
+This is **bolded** paragraph
+
+This is another paragraph with _italic_ text and `code` here
+This is the same paragraph on a new line
+
+- This is a list
+- with items
+"""
+        blocks = markdownToBlocks(md)
+        self.assertEqual(
+            blocks,
+            [
+                "This is **bolded** paragraph",
+                "This is another paragraph with _italic_ text and `code` here\nThis is the same paragraph on a new line",
+                "- This is a list\n- with items",
+            ],
+        )
+
+    def test_blockTypes(self):
+        md = """
+This is **bolded** paragraph
+
+####This is another paragraph with _italic_ text and `code` here
+This is the same paragraph on a new line
+
+- This line
+1. is mismatched
+> and should
+be a 
+- paragraph
+
+- This is a list
+- with items
+
+1. this is an
+2. ordered
+3. list
+
+```
+This is a code block
+that is spanning
+multiple lines
+```
+
+>this is
+>a quote
+>block
+"""
+        blocks = markdownToBlocks(md)
+        blockTypes = []
+        for block in blocks:
+            blockTypes.append(blockToBlockType(block))
+        self.assertEqual(
+            blockTypes,
+            [
+                BlockType.PARAGRAPH,
+                BlockType.HEADING,
+                BlockType.PARAGRAPH,
+                BlockType.UNORDERED_LIST,
+                BlockType.ORDERED_LIST,
+                BlockType.CODE,
+                BlockType.QUOTE,
+            ],
+        )
 
 
 if __name__ == "__main__":
